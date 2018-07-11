29904d33ab852522e2beb4215188ef86422d7c1d
LJ Hapa <leuwendh@bbamastro.com>
2018-07-11 19:16:09 +0800

    initialized UI and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes : Routes = [];
+const routes : Routes = [
+  {
+    path : 'auth',
+    loadChildren : './auth/auth.module#AuthModule'
+  },
+  {
+    path : '',
+    loadChildren : './main/main.module#MainModule'
+  },
+  {
+    path : '**',
+    redirectTo : '/'
+  }
+];
 
 @NgModule( {
   imports : [ RouterModule.forRoot( routes ) ],
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+
+@NgModule( {
+  imports : [
+    CommonModule,
+    AuthRoutingModule
+  ],
+  declarations : [ AuthComponent ]
+} )
+export class AuthModule {
+}
